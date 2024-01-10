d20cc291a4208be4d0e13917715ad025e2b9d19a
ad3rinto <aad3rinto@gmail.com>
2024-01-10 14:38:11 +0000

    adding sunrise data

---
+++ sunrise.py
@@ -0,0 +1,11 @@
+import requests
+
+params = {
+    "lat": 51.507351,
+    "lng": -0.127758
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=params)
+response.raise_for_status()
+data = response.json()
+print(data)
