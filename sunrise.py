772a4e8c4851a59b338464d5cdb0ef789a77fcda
ad3rinto <aad3rinto@gmail.com>
2024-11-08 10:43:38 +0000

    NFU

--- sunrise.py
+++ sunrise.py
@@ -5,7 +5,7 @@ params = {
     "lng": -0.127758
 }
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=params)
+response = requests.get("https://api.sunrise-sunset.org/json", params=params)
 response.raise_for_status()
 data = response.json()
 print(data)
